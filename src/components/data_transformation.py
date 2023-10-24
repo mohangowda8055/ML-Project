77687c58bbb106451a1b7097afe7c35c42f35a6e
Mohan Gowda K <mohangowda8055k@gmail.com>
2023-10-24 16:15:21 +0530

    Data Transformation Completed

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -39,7 +39,7 @@ class DataTransformation:
             num_pipeline=Pipeline(
                 steps=[
                     ("imputer",SimpleImputer(strategy="median")),
-                    ("scaler",StandardScaler(with_mean=False))
+                    ("scaler",StandardScaler())
                 ]
             )
 
@@ -88,7 +88,7 @@ class DataTransformation:
             )
 
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df) 
-            input_feature_test_arr=preprocessing_obj.fit_transform(input_feature_test_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
 
             train_arr=np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
             test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
